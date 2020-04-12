b6aa1f9f5e4a9620a0c0fe90e25917c2cbfa0b77
SnusnumrConceit <snusnumr1996@gmail.com@>
2020-04-12 19:34:52 +0400

    Добавлены переводы на стороне Backend

---
+++ resources/lang/ru/orders.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: snusnumr
+ * Date: 12.04.20
+ * Time: 13:46
+ */
---
+++ resources/lang/ru/users.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: snusnumr
+ * Date: 12.04.20
+ * Time: 14:14
+ */
